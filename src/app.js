5abfd1258ddbbb6fa6f35bcfe0d526fe6338b760
Omollo Cliff Oganda <cliffosego2000@gmail.com>
2022-11-24 01:07:59 +0300

    search params and hooks, useState

--- src/app.js
+++ src/app.js
@@ -1,35 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
-import Pet from "./pet";
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, props.name),
-    React.createElement("h2", {}, props.animal),
-    React.createElement("h2", {}, props.breed),
-  ]);
-};
-
+import SearchParams from "./SearchParams";
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", { id: "brand" }, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese",
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockteil",
-    }),
-    React.createElement(Pet, {
-      name: "Doink",
-      animal: "Cat",
-      breed: "Mix",
-    }),
-  ]);
-};
+  return(
+    <div id="my-app">
+      <h1>Adopt Me!</h1>
+      <SearchParams/>
+    </div>
+  )
+}
 
-const myRenderedApp = React.createElement(App);
-render(myRenderedApp, document.getElementById("root"));
+render(<App/>, document.getElementById("root"))
